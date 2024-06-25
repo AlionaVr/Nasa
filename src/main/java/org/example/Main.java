e1d3e19151d7e2807fe897d3bb3e5d13e5fe4911
AlionaVr <alonavershilo@yandex.by>
2024-06-25 18:49:47 +0300

    Creation Telegram bot, that repeats what you write to him

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,28 +14,28 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String url = "https://api.nasa.gov/planetary/apod" +
-                "?api_key=mxsSItaWLL3vxJcO5Fhwz20cmyZZta7grFaTTUT9" +
-                "&date=2024-06-20";
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-        CloseableHttpResponse response = httpclient.execute(httpGet);
-
-        // Scanner scanner = new Scanner(response.getEntity().getContent());
-        //System.out.println(scanner.nextLine());
-
-        ObjectMapper mapper = new ObjectMapper();
-        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(), NasaAnswer.class);
-
-        String imageUrl = answer.url;
-        String[] splittedUrl = imageUrl.split("/");
-        String fileName = splittedUrl[splittedUrl.length - 1];
-        HttpGet imageGet = new HttpGet(imageUrl);
-        CloseableHttpResponse image = httpclient.execute(imageGet);
-
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-        image.getEntity().writeTo(fileOutputStream);
-
+    public static void main(String[] args) throws IOException, TelegramApiException {
+//        String url = "https://api.nasa.gov/planetary/apod" +
+//                "?api_key=mxsSItaWLL3vxJcO5Fhwz20cmyZZta7grFaTTUT9" +
+//                "&date=2024-06-20";
+//        CloseableHttpClient httpclient = HttpClients.createDefault();
+//        HttpGet httpGet = new HttpGet(url);
+//        CloseableHttpResponse response = httpclient.execute(httpGet);
+//
+//        // Scanner scanner = new Scanner(response.getEntity().getContent());
+//        //System.out.println(scanner.nextLine());
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        NasaAnswer answer = mapper.readValue(response.getEntity().getContent(), NasaAnswer.class);
+//
+//        String imageUrl = answer.url;
+//        String[] splittedUrl = imageUrl.split("/");
+//        String fileName = splittedUrl[splittedUrl.length - 1];
+//        HttpGet imageGet = new HttpGet(imageUrl);
+//        CloseableHttpResponse image = httpclient.execute(imageGet);
+//
+//        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+//        image.getEntity().writeTo(fileOutputStream);
+        new MyTelegramBot("Ellionasabot", "7369611649:AAGw6DmhpzOhcNyl9rP1IeQv-zl5Usgaubk");
     }
 }
---
+++ src/main/java/org/example/MyTelegramBot.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyTelegramBot extends TelegramLongPollingBot {
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+    public MyTelegramBot(String BOT_NAME, String BOT_TOKEN) throws TelegramApiException {
+        this.BOT_NAME = BOT_NAME;
+        this.BOT_TOKEN = BOT_TOKEN;
+
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot (this);
+
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        // TODO
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage();
+            long chatId = update.getMessage().getChatId();
+            message.setChatId(chatId);
+            String text = update.getMessage().getText();
+            message.setText("Вы написали:" + text);
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // TODO
+        return BOT_NAME;
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return BOT_TOKEN;
+    }
+
+}
+
